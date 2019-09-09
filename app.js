fb7559be81b6a40819452fbab0bbc7e667b3c46a
ABIZEYIMANA Victor <svicky.shema@gmail.com>
2019-09-10 01:32:39 +0200

    Chore(database) Setting Server
    Setting Server
    Sign up
    sign in
    can change a user to mentor
    can view all users
    
    [start #168362228][start #168362249][start #168362248]

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
 import http from 'http';
 import express from 'express';
 import logger from 'morgan';
-import bodyParser from 'body-parser'; // parsing bodies
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
 import swaggerUi from 'swagger-ui-express';
+import db from './server/db'
+
+new db().createTables();
 
 import routes from './server/routes/index';
 import myError from './server/middleware/error';
 import swaggerDocument from './swagger.json';
 
-
 const port = process.env.PORT || 3000;
-const app = express(); // setup express application
+const app = express(); 
 const server = http.createServer(app);
 
-app.use(logger('dev')); // log requests to the console
+dotenv.config();
 
-// Parse incoming requests data
+app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Importing API URL's
-// app.use('/api/v1', routes);
 app.use('/', routes);
-// Swagger API
+
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-// Importing API URL's
 app.use(myError.handle404);
 app.use(myError.otherErrors);
 
